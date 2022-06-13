b89ffa27a15e0a8687a3b2b5f32d22d85b088c38
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-13 03:12:52 -0300

    feat: getters & setters servico

--- src/main/java/br/com/veterinaria/model/entidade/Servico.java
+++ src/main/java/br/com/veterinaria/model/entidade/Servico.java
@@ -1,6 +1,27 @@
 package br.com.veterinaria.model.entidade;
 
 public class Servico {
-	private int 	idServico;
+	private long 	idServico;
 	private String 	servico;
+	private long	idFicha;
+	
+	
+	public long getIdServico() {
+		return idServico;
+	}
+	public String getServico() {
+		return servico;
+	}
+	public long getIdFicha() {
+		return idFicha;
+	}
+	public void setIdServico(long idServico) {
+		this.idServico = idServico;
+	}
+	public void setServico(String servico) {
+		this.servico = servico;
+	}
+	public void setIdFicha(long idFicha) {
+		this.idFicha = idFicha;
+	}
 }
