6bf992a56895623a88e367145993e18f4c35e81d
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-12 21:45:24 -0300

    feat: testes de cadastro e listagem de animais-ftsio

---
+++ testes/br/com/veterinaria/modeo/dao/AnimalDaoTest.java
@@ -0,0 +1,75 @@
+package br.com.veterinaria.modeo.dao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.com.veterinaria.model.dao.AnimalDao;
+import br.com.veterinaria.model.entidade.Animal;
+import br.com.veterinaria.model.entidade.DonoAnimal;
+import br.com.veterinaria.model.entidade.Raca;
+
+class AnimalDaoTest {
+
+	@Test
+	//@Disabled
+	@DisplayName("Teste lista")
+	void test() {
+		Animal a = new Animal();
+		AnimalDao dao = new AnimalDao();
+		ArrayList<Animal> lista = new ArrayList<Animal>();
+		
+		lista = dao.listar("");
+		
+		for(Animal animal : lista) {
+			System.out.println(
+		animal.getId_animal()+" - "+
+		animal.getNome()+" - "+
+		animal.getSexo()+" - "+
+		animal.getNascimento()+" - "+
+		animal.getObservacoes()+" - "+
+		animal.getRaca().getIdRaca()+" - "+
+		animal.getRaca().getNomeRaca()+ " - "+
+		animal.getRaca().getNomeEspecie()+" - "+
+		animal.getRaca().getIdEspecie()+" - "+
+		animal.getDono().getNome()+" - "+
+		animal.getId_dono()+" - "+
+		animal.getDono().getId_dono()
+		);
+		}
+		
+		
+	}
+	
+	@Test
+	@DisplayName("Teste Cadastro")
+	void testeincluir() {
+		Animal a = new Animal();
+		AnimalDao dao = new AnimalDao();
+		Raca r = new Raca();
+		DonoAnimal d = new DonoAnimal();
+		
+				
+		a.setId_dono(1);
+		a.setNome("Pochi");
+		a.setSexo("F");
+
+				
+		a.setObservacoes("nada a declarar");
+		
+		r.setIdRaca(30);
+		a.setNascimento("2022-10-10");
+		a.setRaca(r);
+		
+		
+		dao.incluir(a);
+		
+	}
+
+}
