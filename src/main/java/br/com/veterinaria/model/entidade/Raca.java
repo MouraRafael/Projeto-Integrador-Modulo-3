0b1fa49079a603b195fdffb77d7575963999a337
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-12 21:44:16 -0300

    feat: getters e setters criados

--- src/main/java/br/com/veterinaria/model/entidade/Raca.java
+++ src/main/java/br/com/veterinaria/model/entidade/Raca.java
@@ -3,6 +3,32 @@ package br.com.veterinaria.model.entidade;
 public class Raca {
 	private long idRaca;
 	private long idEspecie;
+	
+	
+	public long getIdRaca() {
+		return idRaca;
+	}
+	public long getIdEspecie() {
+		return idEspecie;
+	}
+	public String getNomeRaca() {
+		return nomeRaca;
+	}
+	public String getNomeEspecie() {
+		return nomeEspecie;
+	}
+	public void setIdRaca(long idRaca) {
+		this.idRaca = idRaca;
+	}
+	public void setIdEspecie(long idEspecie) {
+		this.idEspecie = idEspecie;
+	}
+	public void setNomeRaca(String nomeRaca) {
+		this.nomeRaca = nomeRaca;
+	}
+	public void setNomeEspecie(String nomeEspecie) {
+		this.nomeEspecie = nomeEspecie;
+	}
 	private String nomeRaca;
 	private String nomeEspecie;
 	
