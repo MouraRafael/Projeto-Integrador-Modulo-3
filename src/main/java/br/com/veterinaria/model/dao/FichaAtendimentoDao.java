7ca635cfd3442154d2ad22ae0d00ac00231de432
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-13 01:27:48 -0300

    feat: criacao do metodo e teste de listar fichas

--- src/main/java/br/com/veterinaria/model/dao/FichaAtendimentoDao.java
+++ src/main/java/br/com/veterinaria/model/dao/FichaAtendimentoDao.java
@@ -1,9 +1,15 @@
 package br.com.veterinaria.model.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import br.com.veterinaria.model.entidade.Animal;
+import br.com.veterinaria.model.entidade.DonoAnimal;
 import br.com.veterinaria.model.entidade.FichaAtendimento;
+import br.com.veterinaria.model.entidade.Raca;
+import br.com.veterinaria.model.entidade.Veterinario;
 
 public class FichaAtendimentoDao extends Conexao {
 	
@@ -37,5 +43,75 @@ public class FichaAtendimentoDao extends Conexao {
 		
 		return mensagem;
 		
+	}
+
+
+	public ArrayList<FichaAtendimento> listar (String nomeBusca){
+		ArrayList<FichaAtendimento> lista = new ArrayList<FichaAtendimento>();
+		
+		String sql = "SELECT DISTINCT "
+				+ "id_ficha, "
+				+ "data_visita, "
+				+ "especie, "
+				+ "nome_animal, "
+				+ "Veterinario, "
+				+ "nome_dono, "
+				+ "motivo_visita "
+				+ "FROM tudo_ficha  "
+				+ "WHERE nome_animal LIKE ?  "
+				+ "OR nome_dono LIKE ? "
+				+ "OR id_ficha = ?";
+
+		try {
+			PreparedStatement ps = getConexao().prepareStatement(sql);
+			ps.setString(1, "%"+nomeBusca+"%");
+			ps.setString(2, "%"+nomeBusca+"%");
+			ps.setString(3, nomeBusca);
+
+			ResultSet rs = ps.executeQuery();
+
+			FichaAtendimento f = null;
+			Veterinario v = null;
+			Animal a = null;
+			Raca r = null;
+			DonoAnimal d = null;
+
+			while(rs.next()){
+				r = new Raca();
+				a = new Animal();
+				d = new DonoAnimal();
+				v = new Veterinario();
+				f = new FichaAtendimento();
+				
+				f.setIdFicha(rs.getLong("id_ficha"));
+				f.setData(rs.getDate("data_visita"));
+				
+				r.setNomeEspecie(rs.getString("especie"));
+				a.setNome(rs.getString("nome_animal"));
+				a.setRaca(r);
+				f.setAnimal(a);
+				
+				v.setNome(rs.getString("Veterinario"));
+				f.setVeterinario(v);
+
+				d.setNome(rs.getString("nome_dono"));
+				f.setDono(d);
+				
+				f.setMotivoVisita(rs.getString("motivo_visita"));
+				
+
+				lista.add(f);
+
+				
+
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			fecharConexao();
+		}
+
+		return lista;
 	}
 }
--- testes/br/com/veterinaria/modeo/dao/FichaAtendimentoTeste.java
+++ testes/br/com/veterinaria/modeo/dao/FichaAtendimentoTeste.java
@@ -2,6 +2,9 @@ package br.com.veterinaria.modeo.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import br.com.veterinaria.model.dao.FichaAtendimentoDao;
@@ -12,6 +15,7 @@ import br.com.veterinaria.model.entidade.Veterinario;
 class FichaAtendimentoTeste {
 
 	@Test
+	@Disabled
 	void cadastro() {
 		FichaAtendimentoDao dao = new FichaAtendimentoDao(); 
 		FichaAtendimento f = new FichaAtendimento();
@@ -27,6 +31,28 @@ class FichaAtendimentoTeste {
 		
 		assertEquals("sucesso",dao.incluir(f));
 		
+	}
+	
+	
+	@Test
+	void listagem() {
+		FichaAtendimentoDao dao = new FichaAtendimentoDao(); 
+		
+		ArrayList<FichaAtendimento> lista = new ArrayList<FichaAtendimento>();
+		
+		lista = dao.listar("");
+		System.out.println("\n");
+		for(FichaAtendimento f : lista) {
+			System.out.println(
+			f.getIdFicha() + " - "+
+			f.getData() + " - "+
+			f.getAnimal().getRaca().getNomeEspecie()+" - "+
+			f.getAnimal().getNome()+" - "+
+			f.getVeterinario().getNome()+" - "+
+			f.getDono().getNome()+" - "+
+			f.getMotivoVisita()
+					);
+		}
 	}
 
 }
