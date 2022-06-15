589d7bb5c2491d671e20a59b513ebb394db31cb9
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-14 22:20:18 -0300

    feat: Controller para veterinario

---
+++ src/main/java/br/com/veterinaria/model/controller/VeterinarioController.java
@@ -0,0 +1,22 @@
+package br.com.veterinaria.model.controller;
+
+import java.util.ArrayList;
+
+import br.com.veterinaria.model.dao.VeterinarioDao;
+import br.com.veterinaria.model.entidade.Veterinario;
+
+public class VeterinarioController {
+		public String incluir (Veterinario v) {
+			VeterinarioDao dao = new VeterinarioDao();
+			
+			return dao.incluir(v);
+		}
+		
+		public ArrayList<Veterinario> listarAtendimento(){
+			VeterinarioDao dao = new VeterinarioDao();
+			
+			return dao.listarParaAtendimento();
+		}
+}
+
+
