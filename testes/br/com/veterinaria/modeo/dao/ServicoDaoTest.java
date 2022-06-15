3523fbd4662d65126a03911b341bc9a1dcfb1731
Rafael Moura <rafael.g.moura@hotmail.com>
2022-06-14 23:53:33 -0300

    feat: teste excluir servicoDao

--- testes/br/com/veterinaria/modeo/dao/ServicoDaoTest.java
+++ testes/br/com/veterinaria/modeo/dao/ServicoDaoTest.java
@@ -1,5 +1,6 @@
 package br.com.veterinaria.modeo.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -35,7 +36,7 @@ class ServicoDaoTest {
 		ArrayList<Servico> listao = new ArrayList<Servico>();
 		
 		
-		listao = dao.listarServicosPrestados(1);
+		listao = dao.listarServicosPrestados(2);
 		System.out.println("\n");
 		
 		for(Servico s : listao) {
@@ -43,6 +44,18 @@ class ServicoDaoTest {
 		}
 		
 		assertTrue(listao.size()>0);
+	}
+	
+	@Test
+	//@Disabled
+	void excluirServico() {
+		Servico s = new Servico();
+		
+		s.setIdFicha(2);
+		s.setIdServico(3);
+		
+		
+		assertEquals("sucesso",dao.excluir(s));
 	}
 
 }
